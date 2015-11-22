f4d385d56efe285a9341ee5db6a3dd36ec00a585
Bruce Becker <brucellino@gmail.com>
2015-11-22 23:56:37 +0200

    Initial commit of zlib

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
+
+module load ci
+echo ${SOFT_DIR}
+mkdir -p ${SOFT_DIR}
+echo ${WORKSPACE}
+mkdir -p ${WORKSPACE}
+echo ${SRC_DIR}
+mkdir -p ${SRC_DIR}
+echo ${NAME}
+echo ${VERSION}
+#module load gcc/4.8.2
+if [[ -s ${SRC_DIR}/${SOURCE_FILE} ]]
+then
+  mkdir -p ${SRC_DIR}
+	wget http://zlib.net/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+fi
+
+tar xvzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
+ls ${WORKSPACE}
+
+cd ${WORKSPACE}/${NAME}-${VERSION}
+./configure --prefix=${SOFT_DIR}
+make -j 8
